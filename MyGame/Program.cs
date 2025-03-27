163e9949865f87704cdb4582d621341a189a4990
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-27 10:55:05 +0000

    Updated code for MyGame

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks.Dataflow;
 
 namespace MyGame
 {
@@ -7,11 +8,11 @@ namespace MyGame
         private string name;
         private float health;
         private float shield;
-        public Enemy(string name)
+        public Enemy(string name, float health, float shield)
         {
-            this.name = name;
-            health = 100;
-            shield = 0;
+            SetName(name);
+            this.health = health;
+            this.shield = shield;
         }
 
         public string GetName()
@@ -19,21 +20,53 @@ namespace MyGame
             return name;
         }
 
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public void SetName(string newName)
+        {
+            if (newName.Length > 8)
+            {
+                newName = newName.Substring(0, 8);
+            }
+            name = newName;
+        }
+
         public void TakeDamage(float damage)
         {
             shield -= damage;
             if (shield < 0)
             {
-               float damageStillToInflict = -shield;
-               shield = 0;
-               health -= damageStillToInflict;
-               if(health < 0) health = 0;
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
             }
         }
 
         private static void Main(string[] args)
         {
+            int numEnemies = int.Parse(args[0]);
+            Enemy[] enemies = new Enemy[numEnemies];
 
+            for (int i = 0; i < numEnemies; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string name = Console.ReadLine();
+                enemies[i] = new Enemy(name, 100, 0);
+            }
+
+            foreach (var enemy in enemies)
+            {
+                Console.WriteLine($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}");
+            }
         }
     }
 }
