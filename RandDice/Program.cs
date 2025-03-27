706471567b6ed24ea6b12ddd6550fa63e92dd17e
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-27 09:28:27 +0000

    Adding new code to RandDice

---
+++ RandDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int seed = int.Parse(args[1]);
+
+            Random rand = new Random(seed);
+            int soma = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                 soma += rand.Next(1, 7);
+            }
+
+            Console.WriteLine(soma);     
+        }
+    }
+}
