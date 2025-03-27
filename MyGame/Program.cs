ec22dca4b443d55d1873adc645d609646238b35e
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-27 09:54:40 +0000

    Adding constructor to MyGame

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -2,11 +2,21 @@
 
 namespace MyGame
 {
-    public class Program
+    public class Enemy
     {
+        private string name;
+        private float health;
+        private float shield;
+            public Enemy(string name)
+            {
+                this.name = name;
+                health = 100;
+                shield = 0;
+            }
+            
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            
         }
     }
 }
